11c46b2c320df06371baadca95c46794afb57d16
ggrangia <giacomo.grangia@gmail.com>
2022-11-17 20:22:00 +0000

    Update randomselection select

--- pkg/selection/randomselection.go
+++ pkg/selection/randomselection.go
@@ -2,15 +2,13 @@ package selection
 
 import (
 	"math/rand"
-
-	"github.com/ggrangia/lb_go/pkg/backend"
 )
 
 type RandomSelection struct {
 	Seed int64
 }
 
-func (rs *RandomSelection) Select(backends []backend.Backend) backend.Backend {
+func (rs *RandomSelection) Select(l int) int {
 	rand.Seed(rs.Seed)
-	return backends[rand.Intn(len(backends))]
+	return rand.Intn(l)
 }
