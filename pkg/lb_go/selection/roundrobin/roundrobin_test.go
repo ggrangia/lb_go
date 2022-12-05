c81f05985c56ce2929eef446a5759e2c3229f3e8
ggrangia <giacomo.grangia@gmail.com>
2022-12-05 22:21:44 +0000

    Add new RR test no alive

--- pkg/lb_go/selection/roundrobin/roundrobin_test.go
+++ pkg/lb_go/selection/roundrobin/roundrobin_test.go
@@ -154,3 +154,44 @@ func TestServeHttpAlive(t *testing.T) {
 		}
 	}
 }
+
+func TestServeHttpNotAlive(t *testing.T) {
+	teardown, backends := test.SetupBackends(t, 3)
+	defer teardown(t)
+	rr := New()
+	// Backends alive is false by default
+	for i := 0; i < len(backends); i++ {
+		rr.AddBackend(backends[i])
+	}
+
+	tests := []struct {
+		status int
+	}{
+		{status: 500},
+		{status: 500},
+		{status: 500},
+		{status: 500},
+	}
+
+	for _, tc := range tests {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		rr.ServeHTTP(w, r)
+		res := w.Result()
+		defer res.Body.Close()
+		data, err := io.ReadAll(res.Body)
+		raw := string(data)
+		// remove trailing newlines
+		got := strings.TrimRight(raw, "\r\n")
+		if err != nil {
+			t.Errorf("expected error to be nil got %v", err)
+		}
+		if res.StatusCode != tc.status {
+			t.Errorf("expected status code %v got %v", tc.status, res.StatusCode)
+		}
+
+		if diff := cmp.Diff(got, ErrNoServer.Error()); diff != "" {
+			t.Errorf("expected %v got %v, Diff: %v", ErrNoServer.Error(), got, diff)
+		}
+	}
+}
