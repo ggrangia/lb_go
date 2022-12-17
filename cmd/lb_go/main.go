6c6e09d8d6d41cf3ea5fd9b631d8f3396b30d03b
ggrangia <giacomo.grangia@gmail.com>
2022-12-17 11:42:05 +0000

    Setup cobra + viper

--- cmd/lb_go/main.go
+++ cmd/lb_go/main.go
@@ -1,54 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"net/http/httptest"
-	"time"
-
-	"github.com/ggrangia/lb_go/pkg/healthcheck"
-	"github.com/ggrangia/lb_go/pkg/lb_go"
-	"github.com/ggrangia/lb_go/pkg/lb_go/backend"
-	"github.com/ggrangia/lb_go/pkg/lb_go/selection"
-	"github.com/ggrangia/lb_go/pkg/lb_go/selection/randomselection"
-	"github.com/ggrangia/lb_go/pkg/lb_go/selection/roundrobin"
+	"github.com/ggrangia/lb_go/pkg/cmd"
 )
 
 func main() {
 
-	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "this call was relayed by the reverse proxy")
-	}))
-	defer backendServer.Close()
+	cmd.Execute()
+	/*
+		backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "this call was relayed by the reverse proxy")
+		}))
+		defer backendServer.Close()
 
-	backendServer2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "this call was relayed by the reverse proxy2")
-	}))
-	defer backendServer2.Close()
-	backendServer3 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "this call was relayed by the reverse proxy3")
-	}))
-	defer backendServer3.Close()
-	backends := []*backend.Backend{
-		backend.NewBackend(backendServer.URL),
-		backend.NewBackend(backendServer2.URL),
-		backend.NewBackend(backendServer3.URL),
-	}
+		backendServer2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "this call was relayed by the reverse proxy2")
+		}))
+		defer backendServer2.Close()
+		backendServer3 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "this call was relayed by the reverse proxy3")
+		}))
+		defer backendServer3.Close()
+		backends := []*backend.Backend{
+			backend.NewBackend(backendServer.URL),
+			backend.NewBackend(backendServer2.URL),
+			backend.NewBackend(backendServer3.URL),
+		}
 
-	algo := "roundrobin"
-	var selector selection.Selector
-	switch algo {
-	case "roundrobin":
-		selector = roundrobin.NewWithBackends(backends)
-	case "randomselection":
-		selector = randomselection.NewWithBackends(time.Now().UTC().UnixNano(), backends)
-	default:
-		log.Fatalf("Unknown selection algorithm: %v", algo)
-	}
+		algo := "roundrobin"
+		var selector selection.Selector
+		switch algo {
+		case "roundrobin":
+			selector = roundrobin.NewWithBackends(backends)
+		case "randomselection":
+			selector = randomselection.NewWithBackends(time.Now().UTC().UnixNano(), backends)
+		default:
+			log.Fatalf("Unknown selection algorithm: %v", algo)
+		}
 
-	hc := healthcheck.New(selector, 5)
+		hc := healthcheck.New(selector, 5)
 
-	lb := lb_go.NewLb(selector, hc)
-	lb.Start()
+		lb := lb_go.NewLb(selector, hc)
+		lb.Start()
+	*/
 }
---
+++ pkg/cmd/root.go
@@ -0,0 +1,106 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ggrangia/lb_go/pkg/healthcheck"
+	"github.com/ggrangia/lb_go/pkg/lb_go"
+	"github.com/ggrangia/lb_go/pkg/lb_go/backend"
+	"github.com/ggrangia/lb_go/pkg/lb_go/selection"
+	"github.com/ggrangia/lb_go/pkg/lb_go/selection/randomselection"
+	"github.com/ggrangia/lb_go/pkg/lb_go/selection/roundrobin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// Used for flags.
+	cfgFile   string
+	algorithm string
+
+	rootCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start the loab balancer",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+var cmdStart = &cobra.Command{
+	Use:   "start",
+	Short: "Start the loab balancer",
+	Run: func(cmd *cobra.Command, args []string) {
+		algo := viper.GetString("algorithm")
+		fmt.Printf("aaaaa: %v\n", viper.GetString("algorithm"))
+		fmt.Printf("str slice: %v\n", viper.GetStringSlice("backends"))
+		back_urls := viper.GetStringSlice("backends")
+		backends := make([]*backend.Backend, len(back_urls))
+		for i, b := range back_urls {
+			backends[i] = backend.NewBackend(b)
+		}
+		start(backends, algo)
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&algorithm, "algorithm", "a", "default_algo", "load balancing algorithm to be used")
+	rootCmd.PersistentFlags().Int("healthcheck", 5, "load balancing algorithm to be used")
+	viper.BindPFlag("algorithm", rootCmd.PersistentFlags().Lookup("algorithm"))
+	viper.BindPFlag("healthcheck", rootCmd.PersistentFlags().Lookup("healthcheck"))
+
+	rootCmd.AddCommand(cmdStart)
+}
+
+func initConfig() {
+	fmt.Println("AAAAAAAAAAAAAAA")
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		fmt.Printf("HOME: %s\n", home)
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println("Failed reading config file:", err.Error())
+	}
+}
+
+func start(backends []*backend.Backend, algo string) {
+	var selector selection.Selector
+	switch algo {
+	case "roundrobin":
+		selector = roundrobin.NewWithBackends(backends)
+	case "randomselection":
+		selector = randomselection.NewWithBackends(time.Now().UTC().UnixNano(), backends)
+	default:
+		log.Fatalf("Unknown selection algorithm: %v", algo)
+	}
+
+	hc := healthcheck.New(selector, 5)
+
+	lb := lb_go.NewLb(selector, hc)
+	lb.Start()
+}
