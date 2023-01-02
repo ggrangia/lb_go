5f4174aeb1fe77e03b45f5c0cd43cc28eecb8ad9
ggrangia <giacomo.grangia@gmail.com>
2023-01-02 16:12:00 +0000

    Implement heap interface - wrr

--- pkg/lb_go/selection/wrr/wrr.go
+++ pkg/lb_go/selection/wrr/wrr.go
@@ -1,22 +1,57 @@
 package wrr
 
 import (
+	"container/heap"
 	"sync"
 
 	"github.com/ggrangia/lb_go/pkg/lb_go/backend"
 )
 
+type weightedBackends struct {
+	backend.Backend
+	weight int
+}
+
 type Wrr struct {
 	mutex    sync.RWMutex
-	Backends []*backend.Backend
+	Backends []*weightedBackends
 }
 
 func New() *Wrr {
 	return &Wrr{}
 }
 
-func NewWithBackends(backends []*backend.Backend) *Wrr {
-	return &Wrr{
-		Backends: backends,
+func (w *Wrr) AddWeightedBackend(b *backend.Backend, i int) {
+	w.mutex.Lock()
+	wb := &weightedBackends{*b, i}
+	heap.Push(w, wb)
+	w.mutex.Unlock()
+}
+
+func (w *Wrr) Len() int {
+	return len(w.Backends)
+}
+
+func (w *Wrr) Less(i, j int) bool {
+	return w.Backends[i].weight < w.Backends[j].weight
+}
+
+func (w *Wrr) Swap(i, j int) {
+	w.Backends[i], w.Backends[j] = w.Backends[j], w.Backends[i]
+}
+
+func (w *Wrr) Push(x any) {
+	item, ok := x.(*weightedBackends)
+	if !ok {
+		return
 	}
+	w.Backends = append(w.Backends, item)
+}
+
+func (w *Wrr) Pop() interface{} {
+	l := len(w.Backends)
+	b := w.Backends[l-1]
+	w.Backends[l-1] = nil
+	w.Backends = w.Backends[0 : l-1]
+	return b
 }
